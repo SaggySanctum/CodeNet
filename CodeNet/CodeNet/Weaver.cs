4fbbb2ee9a980f878ad6cee76181ed599e3a0087
feliccot <feliccot@mit.edu>
2014-07-23 16:49:11 -0700

    search alg for the proper list

--- CodeNet/CodeNet/Weaver.cs
+++ CodeNet/CodeNet/Weaver.cs
@@ -9,24 +9,118 @@ namespace CodeNet
     public class Weaver
     {
         private List<Middleware> _middleware;
-        private Node _root;
+        private List _middlewareExe;
         private List<Resource> _input;
         private List<Resource> _output;
 
         public Weaver(List<Middleware> middleware, List<Resource> input, List<Resource> output)
         {
             _middleware = middleware;
-            _root = new Node();
             _input = input;
             _output = output;
+
+            public List<Middleware> standAlone;
+            public List<Middleware> notStandAlone;
+
+            // Determines the stand alone middleware
+            foreach(Middleware availableMiddleware in _middleware)
+            {
+                if(availableMiddleware.In().length == 0)
+                {
+                    standAlone.Add(availableMiddleware);
+                }
+                else
+                {
+                    notStandAlone.Add(availableMiddleware);
+                }
+            }
+
+            // Create the list
+            while(output.length != 0)
+            {
+                // Costs the least so its easiest to check these first
+                foreach(Middleware standAloneMiddleware in standAlone)
+                {
+                    var middlewareUsed = false;
+                    foreach(Resource availableOutput in standAloneMiddleware.Out())
+                    {
+                        Resource requirementToRemove;
+                        foreach(Resource requiredOutput in _output)
+                        {   
+                            if(availableOutput.equals(requiredOutput))
+                            {
+                                requirementToRemove = requiredOutput;
+                                middlewareUsed = true;
+                                break;
+                            }
+                        }
+                        if(requirementToRemove != undefined)
+                        {
+                            _output.Remove(requirementToRemove);
+                        }
+                    }
+                    if(middlewareUsed)
+                    {
+                        _middlewareExe.Add(standAloneMiddleware);
+                        foreach(Resourse newResourse in standAloneMiddleware.Out())
+                        {
+                            _input.Add(newResourse);
+                        }
+                    }
+                    if(output.length == 0)
+                    {
+                        break;
+                    }
+                }
+
+                // Combine Middleware so thatyou can use the one that are not standAlone -- assuming we need more middleware
+                if(output.length =! 0)
+                {
+                    foreach(Middleware multi in notStandAlone)
+                    {
+                        var requirementToRemove;
+                        foreach(Resource needed in _output)
+                        {
+                            if(multi.has(needed))
+                            {
+                                foreach(Middleware single in standAlone)
+                                {
+                                    foreach(Resource giving in single.Out())
+                                    {
+                                        foreach(Resource taking in multi.In())
+                                        {
+                                            if(giving.equals(taking))
+                                            {
+                                                if(!_middlewareExe.contains(multi))
+                                                {
+                                                    _middlewareExe.Add(multi);
+                                                }
+                                                if(!_middlewareExe.contains(single))
+                                                {
+                                                    _middlewareExe.Add(single);
+                                                }
+                                                requirementToRemove = needed;
+                                                break;
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        if(requirementToRemove != undefined)
+                        {
+
+                        }
+                    }
+                }
+            }
         }
 
-        public List<string> Cost(List<Middleware> target)
+        public List<Resource> ResoursesUsed(List<Middleware> target)
         {
-            List<string> result;
-            int cost = 0;
             List<Resource> inputsAvailable = _input;
             List<Resource> outputsToSatisfy = _output;
+            List<Resource> outputsSatisfied;
 
             foreach(middlewareAvailible in target)
             {
@@ -35,12 +129,12 @@ namespace CodeNet
                 var canUseThisResourse = true;
                 if(middlewareAvailible.In().count != 0)
                 {
-                    foreach(reqiredInput in middlewareAvailible.In())
+                    foreach(Resource reqiredInput in middlewareAvailible.In())
                     {
-                        foreach(givenInput in _input)
+                        foreach(Resource givenInput in _input)
                         {
                             var fulfilled = false;
-                            if(reqiredInput == givenInput)
+                            if(reqiredInput.equals(givenInput))
                             {
                                 fulfilled = true;
                                 break;
@@ -59,16 +153,31 @@ namespace CodeNet
                 if(canUseThisResourse){
                     foreach(output in middlewareAvailible.Out())
                     {
-                        
+                        foreach(neededOutput in outputsToSatisfy)
+                        {
+                            if(output.equals(neededOutput))
+                            {
+                                outputsSatisfied.Add(output);
+                                break;
+                            }
+                        }
                     }
                 }
             }
+            return outputsSatisfied;
+        }
 
+        public int cost(List<Middleware> target, List<Resource> outputsSatisfied)
+        {
+            return target.length - outputsSatisfied.length;
         }
 
         public Context Execute(Context context)
         {
-            return _root.Run(context);
+            foreach(middleware in _middlewareExe)
+            {
+                middleware.Run(context);
+            }
         }
 
     }
